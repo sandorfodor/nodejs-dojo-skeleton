698f8f0f5ff8d2318a16c27d867f760ad2712028
Sandor Fodor <hccoder@gmail.com>
2017-09-09 11:20:43 +0200

    Reformat code

--- test/project.test.js
+++ test/project.test.js
@@ -1,11 +1,11 @@
 var assert = require('assert');
 var Project = require('../src/project');
 
-describe('Project tests', function() {
-  describe('#test()', function() {
-    it('should return 1', function() {
-	let project = new Project();
-	assert.equal(1, project.test());
+describe('Project tests', function () {
+    describe('#test()', function () {
+        it('should return 1', function () {
+            let project = new Project();
+            assert.equal(1, project.test());
+        });
     });
-  });
 });
